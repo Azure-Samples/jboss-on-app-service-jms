2e0ab4aed40038eaf079288c2f63cef5684efe69
Jason Freeberg <jafreebe@microsoft.com>
2020-10-19 11:13:08 -0700

    Update setenv-webapp.sh

--- scripts/setenv-webapp.sh
+++ scripts/setenv-webapp.sh
@@ -1,3 +1,6 @@
+
+# Gets the publishing credentials from the web app, and sets a variable named "creds". 
+# the credentials are used later to deploy code to the web app.
 set_creds()
 {
 	response=`az webapp deployment list-publishing-credentials -g $1 --name $2`
