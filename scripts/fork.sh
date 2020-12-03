26aa10dddebe3d10718d994109b3817c81709250
Sandra Ahlgrimm <sandra.kriemann@gmail.com>
2020-12-03 15:28:35 +0100

    Update fork.sh
    correct the path to script files

--- scripts/fork.sh
+++ scripts/fork.sh
@@ -1,4 +1,4 @@
 SCRIPTDIRPATH="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 
-mkdir -p "$SCRIPTDIRPATH/../.scripts"
-cp $SCRIPTDIRPATH/../scripts/* $SCRIPTDIRPATH/../.scripts/
+mkdir -p "$SCRIPTDIRPATH/.scripts"
+cp $SCRIPTDIRPATH/scripts/* $SCRIPTDIRPATH/.scripts/
